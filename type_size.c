adb115b093a79035c2fdf8f4323a5a99790d375e
Victor Mwilu <mwilumuisyo@gmail.com>
2023-10-02 15:47:34 +0300

    different data type sizes

---
+++ type_size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+				//printing the values the size of common data types 
+				printf("size of int = %d\n",sizeof(int));
+				printf("size of float = %d\n",sizeof(float));
+				printf("size of char = %d\n",sizeof(char));
+				printf("size of double = %d\n",sizeof(double));
+				printf("size of long = %d\n",sizeof(long));
+
+return 0;
+}
+
+	
+
